4fccea35c6397f3a6e1202b15fe64cbaa2e4681d
Russel Young <russel_young@hotmail.com>
2015-02-11 14:09:02 +1100

    Start Game Activity.java
    Thread handling all game aspects and currently testing game GUI.

---
+++ StartGameActivity.java
@@ -0,0 +1,253 @@
+/*  
+ * Tic Tac Toe Game for andriod ( SDK Version 4.0+ )
+ * Created by Russel Young, started on 9/2/2015
+ * Start Game Activity thread
+ */
+
+package com.example.myfirstapp;
+
+import java.util.Arrays;
+import java.util.Random;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+
+public class StartGameActivity extends ActionBarActivity {
+
+	//@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_start_game);
+		
+		/* Creates an empty game board
+		 * Randomises to determine a starting player
+		 * Calls turn() to begin game
+		 * Checks for possible winner after 3 turns
+		 * Halts game on winner
+		 */
+				
+		int x = 3;
+		int[][] board = new int[x][x];
+		board[0][0] = 2;
+		board[0][1] = 2;
+		board[0][2] = 2;
+		board[1][0] = 2;
+		board[1][1] = 2;
+		board[1][2] = 2;
+		board[2][0] = 2;
+		board[2][1] = 2;
+		board[2][2] = 2;
+		
+		Random rand = new Random();
+		int player = rand.nextInt(1);
+		turn(player, board);
+					
+				
+	}
+	
+	
+	void turn(int player, int board[][]) {
+		/* player's move placed on game board, currently automated as easy AI vs easy AI 
+		 * ( this is simply a random placement every turn )
+		 * checks that square is empty, if not chooses another square, keeps track of turns,
+		 * updates GUI after move to display the change (currently testing, errors found)
+		 * calls winner() after 3 turns to check for a winner
+		 * halts game if winner found or if the board is full and no more turns are possible
+		 */
+			
+		Random rand = new Random();
+		int positionX = rand.nextInt(3);
+		int positionY = rand.nextInt(3);
+		int turn = 0;
+		int x = 0;
+		int winner = 0;
+		
+		Button myButton1 = (Button) findViewById(R.id.button1);
+		Button myButton2 = (Button) findViewById(R.id.button5);
+		Button myButton3 = (Button) findViewById(R.id.button9);
+		Button myButton4 = (Button) findViewById(R.id.button4);
+		Button myButton5 = (Button) findViewById(R.id.button3);
+		Button myButton6 = (Button) findViewById(R.id.button6);
+		Button myButton7 = (Button) findViewById(R.id.button7);
+		Button myButton8 = (Button) findViewById(R.id.button8);
+		Button myButton9 = (Button) findViewById(R.id.button2);
+		
+		
+		
+		while (winner == 0 && turn <=8){
+			if (board[positionX][positionY] == 2){
+				if (player == 0 ){
+					board[positionX][positionY] = 0;	
+					turn += 1;
+					
+					/*  currently in testing, errors found
+					 * 
+					if(positionX == 0 && positionY == 0){
+						myButton1.setText("O");
+					}
+					else{ 
+						if(positionX == 0 && positionY == 1){
+							myButton2.setText("O");
+						}else{ 
+							if(positionX == 0 && positionY == 2){
+								myButton3.setText("O");
+							}else{ 
+								if(positionX == 1 && positionY == 0){
+									myButton4.setText("O");
+								}else{ 
+									if(positionX == 1 && positionY == 1){
+										myButton5.setText("O");
+									}else{ 
+										if(positionX == 1 && positionY == 2){
+											myButton6.setText("O");
+										}else{ 
+											if(positionX == 2 && positionY == 0){
+												myButton7.setText("O");
+											}else{ 
+												if(positionX == 2 && positionY == 1){
+													myButton8.setText("O");
+												}else{ 
+													if(positionX == 2 && positionY == 2){
+														myButton9.setText("O");
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+					*/
+					
+					if (turn > 1){															
+						winner = wincheck(board,player,turn);
+					}
+					player = 1;
+					
+				}
+				else{
+					board[positionX][positionY] = 1;
+					turn += 1;
+					
+
+					/*  currently in testing, errors found
+					 * 
+					if(positionX == 0 && positionY == 0){
+						myButton1.setText("X");
+					}
+					else{ 
+						if(positionX == 0 && positionY == 1){
+							myButton2.setText("X");
+						}else{ 
+							if(positionX == 0 && positionY == 2){
+								myButton3.setText("X");
+							}else{ 
+								if(positionX == 1 && positionY == 0){
+									myButton4.setText("X");
+								}else{ 
+									if(positionX == 1 && positionY == 1){
+										myButton5.setText("X");
+									}else{ 
+										if(positionX == 1 && positionY == 2){
+											myButton6.setText("X");
+										}else{ 
+											if(positionX == 2 && positionY == 0){
+												myButton7.setText("X");
+											}else{ 
+												if(positionX == 2 && positionY == 1){
+													myButton8.setText("X");
+												}else{ 
+													if(positionX == 2 && positionY == 2){
+														myButton9.setText("X");
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}*/
+					
+					
+					if (turn > 1){
+						winner = wincheck(board,player,turn);								
+					}
+					player = 0;
+				}
+			}
+			else{
+				positionX = rand.nextInt(3);
+				positionY = rand.nextInt(3);
+			}	
+		}
+			
+		x = turn;	
+		
+	}			
+			
+		 
+	public static int wincheck (int[][] board, int player, int turn){
+		//check for a winner and returns winner status
+		
+		int X = 0;
+		int Y = 0;
+		int winner = 0;
+		
+		if(board[X][Y] == player && board[X][Y+1] == player && board[X][Y+2] == player){
+			winner = 1;
+		}
+		else if(board[X][Y] == player && board[X+1][Y] == player && board[X+2][Y] == player){
+			winner = 1;
+		}
+		else if(board[X][Y+1] == player && board[X+1][Y+1] == player && board[X+2][Y+1] == player){
+			winner = 1;
+		}
+		else if(board[X+1][Y] == player && board[X+1][Y+1] == player && board[X+1][Y+0] == player){
+			winner = 1;
+		}
+		else if(board[X+2][Y] == player && board[X+2][Y+1] == player && board[X+2][Y+2] == player){
+			winner = 1;
+		}
+		else if(board[X][Y+2] == player && board[X+1][Y+2] == player && board[X+2][Y+2] == player){
+			winner = 1;
+		}
+		else if(board[X][Y] == player && board[X+1][Y+1] == player && board[X+2][Y+2] == player){
+			winner = 1;
+		}
+		else if(board[X+2][Y] == player && board[X+1][Y+1] == player && board[X][Y+2] == player){
+			winner = 1;
+			}																	
+	
+		if (winner == 1){
+		}
+		return winner;
+		
+	}
+	
+
+	//@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.start_game, menu);
+		return true;
+	}
+
+//@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+}
+
